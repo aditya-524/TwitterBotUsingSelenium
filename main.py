623c28c44662bcf0ac4306de3fca3ce43e550501
aditya-524 <aditya524@hotmail.com>
2022-12-04 13:27:28 +1030

    Global Variable Declartion and class

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+
+
+PROMISED_DOWN = 50
+PROMISED_UP = 50
+CHROME_DRIVER_PATH = "C:/Users/adity/Development/chromedriver"
+TWITTER_EMAIL = "narutofromdbz@gmail.com"
+TWITTER_PASSWORD = "NarutoisfromDBZ"
+
+class InternetSpeedTwitterBot():
+    def __init__(self):
+
+    def get_internet_speed():
+
+    def tweet_at_provider():
